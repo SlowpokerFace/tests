975c573d6dce56f6085a65e02e2bf93d209b8242
SlowpokerFace <112822397+SlowpokerFace@users.noreply.github.com>
2024-06-03 23:30:21 +0300

    Add files via upload

--- test/test_factorial.py
+++ test/test_factorial.py
@@ -1,23 +1,24 @@
-from src.functions import *
+"""test"""
+import src.functions
 
 
 def test_factorial():
     """test"""
-    assert factorial(5) == 120
-    assert factorial(0) == 1
+    assert src.functions.factorial(5) == 120
+    assert src.functions.factorial(0) == 1
 
 
 def test_hatred_s():
     """test"""
-    assert hatred_s('s is the worst letter') == ' hate!  i hate!  the wor hate! t letter'
+    assert src.functions.hatred_s('s is the worst letter') == ' hate!  i hate!  the wor hate! t letter'
 
 
 def test_destined_tea():
     """test"""
-    assert destined_tea() != 'coffee'
+    assert src.functions.destined_tea() != 'coffee'
 
 
 def test_what_the_animal_says():
     """test"""
-    assert what_the_animal_says('cat') == 'meow'
-    assert what_the_animal_says('dog') != 'meow'
+    assert src.functions.what_the_animal_says('cat') == 'meow'
+    assert src.functions.what_the_animal_says('dog') != 'meow'
